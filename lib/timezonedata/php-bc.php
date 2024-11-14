b8fdeb43ce370ea632c62272716727f16a249f20
Ren Xie Liu <ren.xie@proton.ch>
2024-11-14 13:28:35 +0800

    Fix style

--- lib/timezonedata/php-bc.php
+++ lib/timezonedata/php-bc.php
@@ -31,6 +31,7 @@ return [
     'America/Pangnirtung',
     // Moved to backward in 2023a
     'America/Yellowknife',
+    // Moved to backward in 2024b
     'Asia/Choibalsan',
 
     // Original list
